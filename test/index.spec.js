e2ac409b72fd41644b4cc9df1a0643f4e050ddd9
Chris Hiestand <chrishiestand@gmail.com>
2015-07-29 13:18:10 -0700

    Update index.spec.js
    fix eslint errors

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,11 @@
 import {assert} from 'chai';
 
 describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert(-1 === [1,2,3].indexOf(5), "1 wasn't equal!");
-      assert(-1 === [1,2,3].indexOf(0), "2 wasn't equal!");
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert([1, 2, 3].indexOf(2) === 1, "Could not find 2");
+            assert([1, 2, 3].indexOf(5) === -1, "Found 5, but didn't expect to");
+            assert([1, 2, 3].indexOf(0) === -1, "Found 0, but didn't expect to");
+        });
     });
-  });
 });
