a6a2ed32b54380b9db124ff64b287ae900a4544a
Chris Hiestand <chrishiestand@gmail.com>
2015-10-27 17:29:23 -0700

    basic functionality complete
    checkpoint commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,106 @@
-"use strict";
+import _Gcloud from 'gcloud';
+import _Promise from 'bluebird';
+
+
+export function getDataStore(options) {
+
+    //options.keyFilename is required if you lack the GCE scope
+
+    if (!options.projectId) {
+        throw new Error('a google cloud projectId is required');
+    }
+
+    return _Gcloud.datastore.dataset(options);
+}
+
+/**
+ * Make a datastore key
+ * @param {Object} datastore gcloud-node datastore object
+ * @param {Array} path_list - the datastore path in array form: ['root', 'sub1', 'sub2', 'id']
+ * @returns {Object} datastore key
+ */
+export function makeKey(datastore, path_list, namespace = null) {
+
+    if (!path_list.length) {
+        throw new Error('path list is required');
+    }
+
+    if (path_list.length < 2) {
+        throw new ArgumentError("A path list of 1 item will create an incomplete key");
+    }
+
+    let key = path_list;
+
+    if (namespace) {
+        key = {namespace, path: path_list};
+    }
+
+    return datastore.key(key);
+}
+
+/**
+ * Save multiple objects to datastore using the same method
+ * @param {Object} datastore gcloud-node datastore object
+ * @param {Object[]} entities in form [{key, data}, ..]
+ * @param {string} method, one of: insert, update, upsert (default: insert)
+ * @returns {Promise} resolving to apiResponse
+ */
+export function save_P(datastore, entities, method='insert') {
+    return _Promise.promisify(datastore[method], datastore)(entities);
+}
+
+/**
+ * Get multiple objects from datastore
+ * @param {Object} datastore gcloud-node datastore object
+ * @param {string|string[]} keys
+ * @returns {Promise} resolving to apiResponse
+ */
+export function get_P(datastore, keys) {
+    return _Promise.promisify(datastore.get, datastore)(keys);
+}
+
+/**
+ * Delete multiple objects from datastore
+ * @param {Object} datastore gcloud-node datastore object
+ * @param {string|string[]} keys
+ * @returns {Promise} resolving to apiResponse
+ */
+export function delete_P(datastore, keys) {
+    return _Promise.promisify(datastore.delete, datastore)(keys);
+}
+
+/**
+ * Create datastore query
+ * @param {Object} datastore gcloud-node datastore object
+ * @param {string} kind
+ * @param {string} [namespace] optional namespace
+ * @param {bool} [auto_paginate] set false to manually page through results (default: true)
+ * @returns {Promise} resolving to apiResponse
+ */
+export function createQuery(datastore, kind, namespace = null, auto_paginate = true) {
+    return datastore.createQuery(namespace, kind).autoPaginate(auto_paginate);
+}
+
+/**
+ * Run datastore query
+ */
+export function runQuery(transaction, query, callback = null) {
+    //callback = function(err, entities, nextQuery, apiResponse)
+    return transaction.runQuery(query, callback);
+}
+
+// Error Helpers
+
+function ArgumentError() {
+    var temp = Error.apply(this, arguments);
+    temp.name = this.name = 'ArgumentError';
+    this.stack = temp.stack;
+    this.message = temp.message;
+}
+ArgumentError.prototype = Object.create(Error.prototype, {
+    constructor: {
+        value: ArgumentError,
+        writable: true,
+        configurable: true
+    }
+});
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,161 @@
 "use strict";
 
 import _Assert from 'assert';
+import _Fs from 'fs';
+import * as _Oakds from '../src';
+import _Ramda from 'ramda';
 
-describe('Array', function() {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            _Assert([1, 2, 3].indexOf(2) === 1, "Could not find 2");
-            _Assert([1, 2, 3].indexOf(5) === -1, "Found 5, but didn't expect to");
-            _Assert([1, 2, 3].indexOf(0) === -1, "Found 0, but didn't expect to");
+
+let valueEqual     = _Assert.deepStrictEqual;
+let _auth_filename = process.env.GCLOUD_AUTH_FILE || __dirname + '/auth-secret.json';
+let _use_auth_file = _Fs.existsSync(_auth_filename);
+let _project_id    = process.env.GCLOUD_PROJECT_ID || read_project_id_from_file(__dirname + '/auth-project.json');
+
+
+function read_project_id_from_file(filename) {
+
+    if (!_Fs.existsSync(filename)) {
+        console.error('a project id is required');
+        process.exit(99);
+    }
+    return _Fs.readFileSync(filename, {encoding: 'utf8'}).trim();
+}
+
+function get_init_options() {
+    if (_use_auth_file) {
+        return { projectId: _project_id, keyFile: _auth_filename };
+    }
+    return { projectId: _project_id };
+}
+
+let datastore;
+let namespace = 'oakdatastore_test';
+
+describe('oakdatastore', function() {
+
+    this.slow(3000);
+    this.timeout(10000);
+
+    describe('#getDatastore()', () => {
+        it('should return a datastore object', () => {
+            datastore = _Oakds.getDataStore(get_init_options());
+            _Assert(datastore);
         });
     });
+
+    describe('#createQuery()', () => {
+        it('should return a query object', () => {
+            let query = _Oakds.createQuery(datastore, 'kind');
+            _Assert(query);
+            let ns_query = _Oakds.createQuery(datastore, 'kind', namespace);
+            _Assert(ns_query);
+        });
+    });
+
+    describe('#makeKey()', () => {
+        it('should throw an Error', () => {
+            _Assert.throws(() => {_Oakds.makeKey(datastore, []);},
+                            (err) => {return err.name === 'Error';});
+        });
+
+        it('should throw a custom error', () => {
+            _Assert.throws(() => {_Oakds.makeKey(datastore, ['incomplete']);},
+                            (err) => {return err.name === 'ArgumentError';});
+        });
+
+        it('should return a datastore key', () => {
+            let key = _Oakds.makeKey(datastore, ['path', 'id']);
+            _Assert(key);
+        });
+
+        it('should return a datastore key', () => {
+            let key = _Oakds.makeKey(datastore, ['path', 'sub', 'id']);
+            _Assert(key);
+        });
+
+        it('should return a namespaced datastore key', () => {
+            let key = _Oakds.makeKey(datastore, ['path', 'id'], namespace);
+            _Assert(key);
+        });
+    });
+
+    describe('#save_P(), #get_P() and #delete_P()', () => {
+
+        let key;
+
+        before('makekey', () => {
+            key  = _Oakds.makeKey(datastore, ['test_oakdatastore', 'id1'], namespace);
+        });
+
+        after('delete', async () => {
+            await _Oakds.delete_P(datastore, key);
+        });
+
+        it('should save to datastore and read from datastore', async () => {
+
+            let data = {testy: "This is setting testy"};
+
+            await _Oakds.save_P(datastore, [{key, data}]);
+
+            let result = await _Oakds.get_P(datastore, key);
+
+            valueEqual(result.data, data);
+            valueEqual(result.key, key);
+        });
+    });
+
+    describe('#save_P(), #get_P() and #delete_P() multiple', () => {
+        let first_key;
+        let first_data;
+        let second_keys = [];
+        let second_keydata;
+        let test_data = {testy: "This is setting testy"};
+
+        before('makekey', () => {
+
+            first_key    = _Oakds.makeKey(datastore, ['test_oakdatastore', 'id1'], namespace);
+            first_data = {key: first_key, data: test_data};
+
+            second_keydata = _Ramda.range(0,10).map((i) => {
+                let key = _Oakds.makeKey(datastore, ['test_oakdatastore', `id${i}`], namespace);
+                second_keys.push(key);
+                return {key, data: test_data};
+            });
+        });
+
+        afterEach('delete', async () => {
+            await _Oakds.delete_P(datastore, first_key);
+        });
+
+        after('delete', async () => {
+            await _Oakds.delete_P(datastore, second_keys);
+        });
+
+        it('should fail to insert a set of keys because of a duplicate key', async (done) => {
+
+            await _Oakds.save_P(datastore, [first_data]);
+
+            _Oakds.save_P(datastore, second_keydata, 'insert')
+            .then(() => {
+                done(new Error('should have thrown an error when overwriting key'));
+            })
+            .catch((err) => {
+                done();
+            });
+        });
+
+        it('should upsert a set of keys despite a duplicate key ', async () => {
+
+            await _Oakds.save_P(datastore, [first_data]);
+            await _Oakds.save_P(datastore, second_keydata, 'upsert');
+
+            let result1 = await _Oakds.get_P(datastore, first_key);
+            let result2 = await _Oakds.get_P(datastore, second_keys);
+
+            valueEqual(result1.data, test_data);
+            valueEqual(result1.key, first_key);
+            valueEqual(result2.length, 10);
+        });
+    });
+
 });
